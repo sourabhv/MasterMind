12defdfc97ad860bd5c2f468a792f88b6deab00e
Sourabh Verma <sourabh.coder@gmail.com>
2014-07-22 15:56:29 +0530

    Update SettingsActivity and its layout

--- src/contagious/games/mastermind/SettingsActivity.java
+++ src/contagious/games/mastermind/SettingsActivity.java
@@ -1,7 +1,30 @@
 package contagious.games.mastermind;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ToggleButton;
 
 public class SettingsActivity extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+	
+	public void soundToggleClick(View view) {
+		boolean soundOn = ((ToggleButton)view).isChecked();
+//		GameEngine.soundSwitch(soundOn);
+	}
+	
+	public void highscoreResetClick(View view) {
+//		DataHandler.getInstance().deleteAll()
+	}
 
 }
