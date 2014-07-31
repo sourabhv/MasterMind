0de8e95b3631ec4540a0d28a5223c84faa0dde18
Sourabh Verma <sourabh.coder@gmail.com>
2014-07-31 22:08:10 +0530

    Remove makePlayarea(...)

--- src/contagious/games/mastermind/GameEngine.java
+++ src/contagious/games/mastermind/GameEngine.java
@@ -3,5 +3,8 @@ package contagious.games.mastermind;
 public class GameEngine {
 
 	public static boolean soundStatus;
+	public int MAX_GUESSES = 10;
+	public int PEGSCOUNT = 6;
+	public int COMBOLENGTH = 4;
 
 }
--- src/contagious/games/mastermind/MastermindActivity.java
+++ src/contagious/games/mastermind/MastermindActivity.java
@@ -7,14 +7,15 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Chronometer;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 
 public class MastermindActivity extends Activity {
 
     Typeface josefinSans;
     Chronometer timer;
-    ScrollView playArea;
+    GameEngine gameEngine;
+    LinearLayout playArea;
     RelativeLayout hotbar;
 
     @Override
@@ -28,36 +29,32 @@ public class MastermindActivity extends Activity {
         josefinSans = Typeface.createFromAsset(getAssets(), "fonts/JosefinSans-SemiBold.ttf");
         timer = ((Chronometer) findViewById(R.id.timer));
         hotbar = (RelativeLayout) findViewById(R.id.hotbar);
-        playArea = (ScrollView) findViewById(R.id.playarea);
+        playArea = (LinearLayout) findViewById(R.id.playarea);
+        gameEngine = new GameEngine();
 
         timer.setTypeface(josefinSans);
-        makePlayArea(this);
         makeHotbar(this);
         timer.start();
-	}
-
-    private void makeHotbar(Context context) {
-    	RelativeLayout.LayoutParams params;
-    	int pegIds[] = {Peg.BLUE, Peg.GREEN, Peg.RED, Peg.WHITE, Peg.YELLOW, Peg.PURPLE};
-    	Peg peg;
-    	final int IDLB = 1000;
-
-    	for (int i = 0; i < pegIds.length; i++) {
-    		peg = new Peg(context, null);
-    		peg.setId(i + IDLB);
-    		peg.setTag("hotbar_" + i);
-    		peg.setDrawableID(pegIds[i]);
-    		params = new RelativeLayout.LayoutParams(
-    				RelativeLayout.LayoutParams.WRAP_CONTENT,
-    				RelativeLayout.LayoutParams.WRAP_CONTENT);
-    		if (i > 0)
-    			params.addRule(RelativeLayout.RIGHT_OF, (i + IDLB) - 1);
-    		hotbar.addView(peg, params);
-    	}
     }
 
-	private void makePlayArea(Context context) {
+    private void makeHotbar(Context context) {
+        RelativeLayout.LayoutParams params;
+        int pegIds[] = {Peg.BLUE, Peg.GREEN, Peg.RED, Peg.WHITE, Peg.YELLOW, Peg.PURPLE};
+        Peg peg;
+        final int IDLB = 1000;
 
-	}
+        for (int i = 0; i < pegIds.length; i++) {
+            peg = new Peg(context, null);
+            peg.setId(i + IDLB);
+            peg.setTag("hotbar_" + i);
+            peg.setDrawableID(pegIds[i]);
+            params = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            if (i > 0)
+                params.addRule(RelativeLayout.RIGHT_OF, (i + IDLB) - 1);
+            hotbar.addView(peg, params);
+        }
+    }
 
 }
