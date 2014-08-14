6a5d31f5783d43477586e19b32dcd50fd367b3f6
Sourabh Verma <sourabh.coder@gmail.com>
2014-08-14 22:30:43 +0530

    Move MastermindActivity layout into class

--- src/contagious/games/mastermind/MastermindActivity.java
+++ src/contagious/games/mastermind/MastermindActivity.java
@@ -22,179 +22,256 @@ import android.widget.RelativeLayout;
 
 public class MastermindActivity extends Activity {
 
-    private class GetHighscores extends AsyncTask<Void, Void, Void> {
-        List<Map<String, Object>> list;
-        @Override
-        protected Void doInBackground(Void... params) {
-            DataHandler dataHandler = DataHandler.getInstance(MastermindActivity.this);
-            list = dataHandler.selectAll();
-            return null;
-        }
+	private class GetHighscores extends AsyncTask<Void, Void, Void> {
+		List<Map<String, Object>> list;
+		@Override
+		protected Void doInBackground(Void... params) {
+			DataHandler dataHandler = DataHandler.getInstance(MastermindActivity.this);
+			list = dataHandler.selectAll();
+			return null;
+		}
 
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            highscores = list;
-        }
-    }
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			highscores = list;
+		}
+	}
 
-    Typeface josefinSans;
-    Chronometer timer;
-    GameEngine gameEngine;
-    RelativeLayout hotbar;
-    LinearLayout playarea;
-    List<Map<String, Object>> highscores;
-    OnClickListener onHotbarClick;
-    int elapsedTime;
+	Typeface josefinSans;
+	Chronometer timer;
+	GameEngine gameEngine;
+	RelativeLayout hotbar;
+	LinearLayout playarea;
+	List<Map<String, Object>> highscores;
+	OnClickListener onHotbarClick;
+	OnClickListener onPegClick;
+	OnClickListener onConfirmClick;
+	int elapsedTime;    
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mastermind);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mastermind);
 
-        new GetHighscores().execute();
-        josefinSans = Typeface.createFromAsset(getAssets(), "fonts/JosefinSans-SemiBold.ttf");
-        timer = ((Chronometer) findViewById(R.id.timer));
-        timer.setBase(SystemClock.elapsedRealtime());
-        hotbar = (RelativeLayout) findViewById(R.id.hotbar);
-        playarea = (LinearLayout) findViewById(R.id.playarea);
-        gameEngine = new GameEngine();
-        elapsedTime = -1;
+		new GetHighscores().execute();
+		josefinSans = Typeface.createFromAsset(getAssets(), "fonts/JosefinSans-SemiBold.ttf");
+		timer = ((Chronometer) findViewById(R.id.timer));
+		timer.setBase(SystemClock.elapsedRealtime());
+		hotbar = (RelativeLayout) findViewById(R.id.hotbar);
+		playarea = (LinearLayout) findViewById(R.id.playarea);
+		gameEngine = new GameEngine();
+		elapsedTime = -1;
 
-        timer.setOnChronometerTickListener(new OnChronometerTickListener() {
+		/* --- listeners --- */
+
+		timer.setOnChronometerTickListener(new OnChronometerTickListener() {
 			@Override
 			public void onChronometerTick(Chronometer arg0) {
 				elapsedTime += 1;
 			}
 		});
 
-        onHotbarClick = new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Peg peg = (Peg) view;
-                if (peg.drawableID() >= Peg.PEGSCOUNT)
-                    peg.setDrawableID(peg.drawableID() - Peg.PEGSCOUNT);
-                else {
-                    int len = hotbar.getChildCount();
-                    for (int i = 0; i < len; i++) {
-                        Peg cPeg = (Peg) hotbar.getChildAt(i);
-                        if (cPeg.drawableID() >= Peg.PEGSCOUNT)
-                            cPeg.setDrawableID(cPeg.drawableID() - Peg.PEGSCOUNT);
-                    }
+		onHotbarClick = new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Peg peg = (Peg) view;
+				if (peg.drawableID() >= Peg.PEGSCOUNT)
+					peg.setDrawableID(peg.drawableID() - Peg.PEGSCOUNT);
+				else {
+					int len = hotbar.getChildCount();
+					for (int i = 0; i < len; i++) {
+						Peg cPeg = (Peg) hotbar.getChildAt(i);
+						if (cPeg.drawableID() >= Peg.PEGSCOUNT)
+							cPeg.setDrawableID(cPeg.drawableID() - Peg.PEGSCOUNT);
+					}
 
-                    peg.setDrawableID(peg.drawableID() + Peg.PEGSCOUNT);
-                }
-            }
-        };
+					peg.setDrawableID(peg.drawableID() + Peg.PEGSCOUNT);
+				}
+			}
+		};
 
-        timer.setTypeface(josefinSans);
-        makeHotbar(this);
-        timer.start();
-    }
+		onPegClick = new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Peg peg = (Peg) view;
+				int active_peg = Peg.NULL, len = hotbar.getChildCount();
+				int playlevel = Integer.parseInt(peg.getTag().toString().substring(8, 9));
+				if (playlevel != gameEngine.guessCount)
+					return;
 
-    private void makeHotbar(Context context) {
-        RelativeLayout.LayoutParams params;
-        int pegIds[] = {Peg.BLUE, Peg.GREEN, Peg.RED, Peg.WHITE, Peg.YELLOW, Peg.PURPLE};
-        Peg peg;
-        final int IDLB = 1000;
+				for (int i = 0; i < len; i++) {
+					Peg cPeg = (Peg) hotbar.getChildAt(i);
+					if (cPeg.drawableID() >= Peg.PEGSCOUNT) {
+						active_peg = cPeg.drawableID();
+						break;
+					}
+				}
 
-        for (int i = 0; i < pegIds.length; i++) {
-            peg = new Peg(context, null);
-            peg.setId(i + IDLB);
-            peg.setTag("hotbar_" + i);
-            peg.setDrawableID(pegIds[i]);
-            peg.setOnClickListener(onHotbarClick);
-            params = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT);
-            if (i > 0)
-                params.addRule(RelativeLayout.RIGHT_OF, (i + IDLB) - 1);
-            hotbar.addView(peg, params);
-        }
-    }
+				if (active_peg != Peg.NULL)
+					active_peg -= Peg.PEGSCOUNT;
 
-    public void onPegClick(View view) {
-        Peg peg = (Peg) view;
-        int active_peg = Peg.NULL, len = hotbar.getChildCount();
-        int playlevel = Integer.parseInt(peg.getTag().toString().substring(8, 9));
-        if (playlevel != gameEngine.guessCount)
-            return;
+				peg.setDrawableID(active_peg);
 
-        for (int i = 0; i < len; i++) {
-            Peg cPeg = (Peg) hotbar.getChildAt(i);
-            if (cPeg.drawableID() >= Peg.PEGSCOUNT) {
-                active_peg = cPeg.drawableID();
-                break;
-            }
-        }
+			}
+		};
 
-        if (active_peg != Peg.NULL)
-            active_peg -= Peg.PEGSCOUNT;
+		onConfirmClick = new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				ImageView confirm = (ImageView) view;
+				int playlevel = Integer.parseInt(confirm.getTag().toString().substring(8, 9));
+				int[] pegCombo = new int[4];
+				if (playlevel != gameEngine.guessCount)
+					return;
 
-        peg.setDrawableID(active_peg);
-    }
+				RelativeLayout parent = (RelativeLayout) confirm.getParent();
 
-    public void onConfirmClick(View view) {
-        ImageView confirm = (ImageView) view;
-        int playlevel = Integer.parseInt(confirm.getTag().toString().substring(8, 9));
-        int[] pegCombo = new int[4];
-        if (playlevel != gameEngine.guessCount)
-            return;
+				for (int i = 4; i < 8; i++) {
+					Peg peg = (Peg) parent.getChildAt(i);
+					pegCombo[i-4] = peg.drawableID();
+					if (peg.drawableID() == Peg.NULL)
+						return;
+				}
 
-        RelativeLayout parent = (RelativeLayout) confirm.getParent();
+				int[] flagCombo = gameEngine.getFlagCombo(pegCombo);
+				boolean win = gameEngine.getWinStatus(flagCombo);
 
-        for (int i = 4; i < 8; i++) {
-            Peg peg = (Peg) parent.getChildAt(i);
-            pegCombo[i-4] = peg.drawableID();
-            if (peg.drawableID() == Peg.NULL)
-                return;
-        }
+				if (win) {
+					timer.stop();
+					final int guesses = gameEngine.guessCount + 1;
 
-        int[] flagCombo = gameEngine.getFlagCombo(pegCombo);
-        boolean win = gameEngine.getWinStatus(flagCombo);
+					boolean ishighscore = gameEngine.checkHighScore(highscores, elapsedTime, guesses);
 
-        if (win) {
-            timer.stop();
-            // final long timeElapsed = (SystemClock.elapsedRealtime() - timer.getBase()) / 1000;
-            final int guesses = gameEngine.guessCount + 1;
+					Intent intent = new Intent(MastermindActivity.this, EndGameActivity.class);
+					intent.putExtra(GameEngine.WIN, GameEngine.TRUE);
 
-            boolean ishighscore = gameEngine.checkHighScore(highscores, elapsedTime, guesses);
+					if (ishighscore) {
+						intent.putExtra(GameEngine.ISHIGHSCORE, GameEngine.TRUE);
+						intent.putExtra(GameEngine.TIME, elapsedTime);
+						intent.putExtra(GameEngine.GUESSES, guesses);
+					} else {
+						intent.putExtra(GameEngine.ISHIGHSCORE, GameEngine.FALSE);
+					}
 
-            Intent intent = new Intent(this, EndGameActivity.class);
-            intent.putExtra(GameEngine.WIN, GameEngine.TRUE);
+					startActivity(intent);
 
-            if (ishighscore) {
-                intent.putExtra(GameEngine.ISHIGHSCORE, GameEngine.TRUE);
-                intent.putExtra(GameEngine.TIME, elapsedTime);
-                intent.putExtra(GameEngine.GUESSES, guesses);
-            } else {
-                intent.putExtra(GameEngine.ISHIGHSCORE, GameEngine.FALSE);
-            }
+				} else {
+					if (gameEngine.guessCount < 9)
+						gameEngine.guessCount++;
+					else {
+						Intent intent = new Intent(MastermindActivity.this, EndGameActivity.class);
+						intent.putExtra(GameEngine.WIN, GameEngine.FALSE);
+						startActivity(intent);
+					}
 
-            startActivity(intent);
+					for (int i = 0; i < 4; i++) {
+						Flag flag = (Flag) parent.getChildAt(i);
+						flag.setDrawableID(flagCombo[i]);
+					}
 
-        } else {
-            if (gameEngine.guessCount < 9)
-                gameEngine.guessCount++;
-            else {
-                Intent intent = new Intent(this, EndGameActivity.class);
-                intent.putExtra(GameEngine.WIN, GameEngine.FALSE);
-                startActivity(intent);
-            }
+					confirm.setImageResource(R.drawable.confirmblank);
+					RelativeLayout r = (RelativeLayout) playarea.getChildAt(9 - playlevel - 1);
+					ImageView nextConfirm = (ImageView) r.getChildAt(8);
+					nextConfirm.setImageResource(R.drawable.confirm);
+				}
+			}
+		};
 
-            for (int i = 0; i < 4; i++) {
-                Flag flag = (Flag) parent.getChildAt(i);
-                flag.setDrawableID(flagCombo[i]);
-            }
+		timer.setTypeface(josefinSans);
+		makePlayarea(this);
+		makeHotbar(this);
+		timer.start();
+	}
 
-            confirm.setImageResource(R.drawable.confirmblank);
-            RelativeLayout r = (RelativeLayout) playarea.getChildAt(9 - playlevel - 1);
-            ImageView nextConfirm = (ImageView) r.getChildAt(8);
-            nextConfirm.setImageResource(R.drawable.confirm);
-        }
-    }
+	private void makePlayarea(Context context) {
+		for (int tagID = 9, ID = 2000; tagID >= 0; tagID--) {
+			LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+					LinearLayout.LayoutParams.WRAP_CONTENT,
+					LinearLayout.LayoutParams.WRAP_CONTENT);
+			RelativeLayout relativeLayout = new RelativeLayout(context);
+			relativeLayout.setPadding(0, 1, 0, 1);
+
+			for (int i = 0; i < 4; i++) {
+				RelativeLayout.LayoutParams rParams = new RelativeLayout.LayoutParams(
+						RelativeLayout.LayoutParams.WRAP_CONTENT,
+						RelativeLayout.LayoutParams.WRAP_CONTENT);
+				Flag flag = new Flag(context, null);
+				flag.setId(ID);
+				flag.setTag("playarea" + tagID + "_flag" + i);
+				if (i == 1)
+					rParams.addRule(RelativeLayout.RIGHT_OF, ID - 1);
+				else if (i == 2)
+					rParams.addRule(RelativeLayout.BELOW, ID - 2);
+				else if (i == 3) {
+					rParams.addRule(RelativeLayout.RIGHT_OF, ID - 1);
+					rParams.addRule(RelativeLayout.BELOW, ID - 2);
+				}
+				relativeLayout.addView(flag, rParams);
+				ID++;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				RelativeLayout.LayoutParams rParams = new RelativeLayout.LayoutParams(
+						RelativeLayout.LayoutParams.WRAP_CONTENT,
+						RelativeLayout.LayoutParams.WRAP_CONTENT);
+				Peg peg = new Peg(context, null);
+				peg.setId(ID);
+				peg.setTag("playarea" + tagID + "_peg" + i);
+				peg.setOnClickListener(onPegClick);
+				if (i == 0)
+					rParams.addRule(RelativeLayout.RIGHT_OF, ID - 3);
+				else
+					rParams.addRule(RelativeLayout.RIGHT_OF, ID - 1);
+				relativeLayout.addView(peg, rParams);
+				ID++;
+			}
+
+			RelativeLayout.LayoutParams rParams = new RelativeLayout.LayoutParams(
+					RelativeLayout.LayoutParams.WRAP_CONTENT,
+					RelativeLayout.LayoutParams.WRAP_CONTENT);
+			rParams.addRule(RelativeLayout.RIGHT_OF, ID - 1);
+			ImageView confirm = new ImageView(context);
+			confirm.setId(ID);
+			confirm.setTag("playarea9" + tagID + "confirm" + tagID);
+			confirm.setOnClickListener(onConfirmClick);
+			if (tagID == 0)
+				confirm.setImageResource(R.drawable.confirm);
+			else
+				confirm.setImageResource(R.drawable.confirmblank);
+			confirm.setContentDescription(getString(R.string.confirm));
+			relativeLayout.addView(confirm, rParams);
+			
+			ID++;
+			playarea.addView(relativeLayout, lParams);
+		}
+	}
+
+	private void makeHotbar(Context context) {
+		RelativeLayout.LayoutParams params;
+		int pegIds[] = {Peg.BLUE, Peg.GREEN, Peg.RED, Peg.WHITE, Peg.YELLOW, Peg.PURPLE};
+		Peg peg;
+		final int IDLB = 1000;
+
+		for (int i = 0; i < pegIds.length; i++) {
+			peg = new Peg(context, null);
+			peg.setId(i + IDLB);
+			peg.setTag("hotbar_" + i);
+			peg.setDrawableID(pegIds[i]);
+			peg.setOnClickListener(onHotbarClick);
+			params = new RelativeLayout.LayoutParams(
+					RelativeLayout.LayoutParams.WRAP_CONTENT,
+					RelativeLayout.LayoutParams.WRAP_CONTENT);
+			if (i > 0)
+				params.addRule(RelativeLayout.RIGHT_OF, (i + IDLB) - 1);
+			hotbar.addView(peg, params);
+		}
+	}
+
+	public void onConfirmClick(View view) {
+	}
 
 }
